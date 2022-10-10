b2680b3ef3fee50656b52c6ed676c5b19d2f9077
damilola dolor <damiloladolor@gmail.com>
2022-10-11 00:11:36 +0100

    more refactoring

--- src/main.rs
+++ src/main.rs
@@ -7,23 +7,22 @@ use std::time::{SystemTime, UNIX_EPOCH};
 
 use uuid::Uuid;
 
-struct TrashConfig {
+struct TrasherConfig {
     trash_dir: String,
     trash_meta_dir: String,
     trash_files_dir: String,
 }
 
-struct Trash {
-    config: TrashConfig,
+struct Trasher {
+    config: TrasherConfig,
 }
 
-// struct MetaData {
-//     file_name: String,
-//     file_path: String,
-//     file_uuid: String,
-//     file_size: u64,
-//     file_deleted_at: u64,
-// }
+struct TrashFileMeta {
+    file_name: String,
+    file_path: String,
+    file_uuid: Uuid,
+    file_deleted_at: u128,
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -34,17 +33,16 @@ fn main() {
         return;
     }
 
-    let cfg = TrashConfig {
+    let trasher = Trasher::new(TrasherConfig {
         trash_dir: String::from("/home/damilola/.rtrash"),
         trash_meta_dir: String::from("/home/damilola/.rtrash/meta"),
         trash_files_dir: String::from("/home/damilola/.rtrash/files"),
-    };
-    let trash = Trash::new(cfg);
+    });
 
     for i in args[1..].iter() {
         let p = Path::new(i);
         if p.exists() {
-            trash.trash(p);
+            trasher.trash(p);
         } else {
             println!("file does not exist");
         }
@@ -53,8 +51,8 @@ fn main() {
 
 // TODO: ensure current directory/parent cannot be removed
 
-impl Trash {
-    fn new(cfg: TrashConfig) -> Trash {
+impl Trasher {
+    fn new(cfg: TrasherConfig) -> Trasher {
         if !Path::new(cfg.trash_dir.as_str()).exists() {
             fs::create_dir(cfg.trash_dir.as_str()).expect("Could not create trash directory");
             fs::create_dir(cfg.trash_meta_dir.as_str())
@@ -62,44 +60,61 @@ impl Trash {
             fs::create_dir(cfg.trash_files_dir.as_str())
                 .expect("Could not create trash files directory");
         }
-        Trash { config: cfg }
+        Trasher { config: cfg }
     }
 
-    fn trash(&self, fpath: &Path) {
+    fn create_meta(&self, fpath: &Path) -> TrashFileMeta {
         // create metadata and save in yml file
-        // metadata consists of: original path, filename, timestamp, internal file id
-        let file_id = Uuid::new_v4().to_string();
-        let filename = fpath.file_name().unwrap().to_str().unwrap();
-        let abs_filepath = fpath.canonicalize().unwrap();
-        let abs_filepath = abs_filepath.to_str().unwrap();
-        let timestamp = get_current_timestamp_milli();
-        println!("timestamp: {}", timestamp);
-        println!("absolute file path: {}", abs_filepath);
-        println!("filename: {}", filename);
-        println!("internal file id: {}", file_id);
+        let meta = TrashFileMeta {
+            file_name: String::from(fpath.file_name().unwrap().to_str().unwrap()),
+            file_path: String::from(fpath.canonicalize().unwrap().to_str().unwrap()),
+            file_uuid: Uuid::new_v4(),
+            file_deleted_at: get_current_timestamp_milli(),
+        };
+
+        // println!("timestamp: {}", meta.file_deleted_at);
+        // println!("absolute file path: {}", meta.file_path);
+        // println!("file name: {}", meta.file_name);
+        // println!("internal file id: {:?}", meta.file_uuid);
+
         // create file to store metadata
-        // save metadata to file in appropriate format and dir
-        let mut meta_file =
-            File::create(Path::new(self.config.trash_meta_dir.as_str()).join(file_id)).unwrap();
+        let mut meta_file = File::create(
+            Path::new(self.config.trash_meta_dir.as_str()).join(meta.file_uuid.to_string()),
+        )
+        .unwrap();
+
         meta_file
             .write_all(
                 format!(
-                    "[meta]\noriginal_path: {}\ntimestamp: {}\nfilename: {}\n",
+                    "[meta]\noriginal_path = {}\ntimestamp = {}\nfilename = {}\n",
                     fpath.canonicalize().unwrap().to_str().unwrap(),
-                    timestamp,
-                    filename
+                    meta.file_deleted_at,
+                    meta.file_name
                 )
                 .as_bytes(),
             )
             .unwrap();
+        meta
+    }
+
+    fn trash(&self, fpath: &Path) {
+        // create metadata and save in yml file
+        let meta = self.create_meta(fpath);
+
         // move file to trash directory
-        fs::copy(
-            abs_filepath,
-            Path::new(self.config.trash_files_dir.as_str()).join(filename),
-        )
-        .unwrap();
+        if fpath.is_dir() {
+            println!("err: cannot move a directory yet");
+            return;
+        } else {
+            fs::copy(
+                meta.file_path.as_str(),
+                Path::new(self.config.trash_files_dir.as_str()).join(meta.file_name.as_str()),
+            )
+            .unwrap();
+        }
+
         // delete original file
-        fs::remove_file(abs_filepath).unwrap();
+        fs::remove_file(meta.file_path).unwrap();
     }
 }
 
